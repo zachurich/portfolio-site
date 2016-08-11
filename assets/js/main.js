3492e584d64715adb2f6ab428007054950801a8e
Zach Urich <zach.urich@integritystl.com>
2016-08-10 21:40:36 -0500

    New animations, styles for work and mobile menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,28 @@ GitHubActivity.feed({
 
 // Click Events for Work Section
 $(document).ready(function() {
-  // smoothScroll function is applied from the document ready function
+  $('.menu-button').on('click', function() {
+    if($('.menu-mobile').hasClass('slideOut')) {
+      $('.menu-mobile').removeClass('slideOut');
+      $('.menu-mobile').addClass('slideUp');
+    } else {
+      $('.menu-mobile').addClass('slideOut');
+      $('.menu-mobile').removeClass('slideUp');
+    }
+  });
   $('.work-thumb').on('click', function() {
     // console.log('Hello');
     $('.work-section').css('left', '-100%');
   //  $('.flex-row').hide(2000);
     $('.project-container').show();
-  })
+  });
   $('.back-button').on('click', function() {
       $('.work-section').css('left', '0%');
       $('.flex-row').show();
       $('.project-container').hide(500);
-  })
+  });
+  $('.read-more').on('click', function() {
+      $('.about-more').toggleClass('grow');
+      $('.read-more').hide();
+  });
 });
